a3c5816fb676cb420f786209baf208d02ab015db
Greg Dennis <greg.dennis@intergen.co.nz>
2017-03-20 17:06:31 +1300

    Missed a method...

--- ThingFilter/IFilter.cs
+++ ThingFilter/IFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ThingFilter
 {
@@ -7,5 +8,6 @@ namespace ThingFilter
 		IFilter<T> MatchOn<TProp>(Func<T, TProp> valueFunc, string tag = null, bool requireTag = false);
 		IFilter<T> CaseSensitive();
 		IFilter<T> SortByRelevance();
+		IEnumerable<T> Apply(IEnumerable<T> collection, string query);
 	}
 }
