98cdb266bc8a1ad490b3fe15944ace6eb9037004
Greg Dennis <gregsdennis@yahoo.com>
2017-10-04 16:17:34 +1300

    updated build script

--- nuget-restore.ps1
+++ nuget-restore.ps1
@@ -3,6 +3,6 @@ $nuget_exe = ".\nuget.exe"
 $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
 Invoke-WebRequest $sourceNugetExe -OutFile $nuget_exe
 
-. $nuget_exe restore "Manatee.Json.sln" -NonInteractive
+. $nuget_exe restore "Filter.sln" -NonInteractive
 
 exit $lastExitCode
