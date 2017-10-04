f7523637fe69e10a6954e44047e2281f6dd151de
Greg Dennis <gregsdennis@yahoo.com>
2017-10-04 16:16:16 +1300

    updated build script

--- build.bat
+++ build.bat
@@ -23,7 +23,7 @@ if exist "%programfiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15
 REM *** NOTE *** When running this locally, use the version without the quotes.
 REM              MyGet requires the quotes, but the command line doesn't like them.  Don't ask me...
 
-call "%msbuild%" ThingFilter.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+call "%msbuild%" Filter.sln /p:Configuration="%config%" /m:1 /v:m /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
 if not "%errorlevel%"=="0" goto failure
 
 REM Run Tests
